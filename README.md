c61ac56857802dafb367c69be4cadb98a98c0b5e
Yacine ANANE <yacine.anane@healius.com.au>
2025-08-30 14:41:48 +0000

    ADDED: invoke_ai.py

---
+++ README.md
@@ -0,0 +1,18 @@
+# Our GovHack 2025 project
+
+## Usage:
+install packages:
+```
+pip install -r requirements.txt
+```
+OR
+```
+conda env create -f environment.yml -n env_name
+conda activate env_name
+```
+
+To use AI:
+create a file named '.env' at the root of the project and write your open AI api key inside. Replace \<API_KEY\> with yours on the template bellow:
+```
+OPENAI_API_KEY=<API_KEY>
+```
--- permits_plots.py
+++ permits_plots.py
@@ -79,6 +79,7 @@ def prepare_data():
 
     return _pc_merged, _geojson_pc
 
+
 # ---------- plotting ----------
 def plot_permits_choropleth(permits_category='All'):
     pc_merged, geojson_pc = prepare_data()
@@ -89,15 +90,6 @@ def plot_permits_choropleth(permits_category='All'):
 
     color_col = 'permit_count' if permits_category == 'All' else f"{permits_category.lower().replace(' ','_')}_count"
 
-    
-    if "STATE_NAME" in pc_merged.columns:
-        pc_merged = pc_merged[pc_merged["STATE_NAME"].str.upper() == "VICTORIA"]
-        geojson_pc = json.loads(pc_merged.to_json())
-    else:
-        pc_merged = pc_merged.cx[141:150, -39:-34]
-        geojson_pc = json.loads(pc_merged.to_json())
-
-  
     fig = px.choropleth_mapbox(
         pc_merged,
         geojson=geojson_pc,
@@ -105,17 +97,11 @@ def plot_permits_choropleth(permits_category='All'):
         color=color_col,
         featureidkey="properties.name",
         mapbox_style="open-street-map",
-        center={"lat": -37.0, "lon": 144.0},   # Victoria center
-        zoom=6,
+        center={"lat": -25.0, "lon": 133.0},
+        zoom=4,
         hover_name='name',
         hover_data={'permit_count': True, 'total_cost': True},
         opacity=0.6,
     )
-
-    fig.update_layout(
-        margin={"r":0,"t":0,"l":0,"b":0},
-        mapbox_bounds={"west": 141, "east": 150, "south": -39, "north": -34}
-    )
-
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
     return fig
-
